ea2b55f9bd61ed4e449afedf6f95f60cacf64e56
yury <yury.antonyb@gmail.com>
2020-11-03 15:25:25 -0400

    classificacao de imagem funcionando

--- ImgClassification/ViewController.swift
+++ ImgClassification/ViewController.swift
@@ -15,27 +15,49 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         super.viewDidLoad()
         //view.backgroundColor = .black
         
+        
         //here is where we start up the camera
         let captureSession = AVCaptureSession()
-        guard let captureDevice = AVCaptureDevice.default(for: .video) else {return}
-        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else {return}
+        captureSession.sessionPreset = .photo
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else {
+            return
+            
+        }
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else {
+            return
+            
+        }
         captureSession.addInput(input)
+        
         captureSession.startRunning()
         
         let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        
         view.layer.addSublayer(previewLayer)
         previewLayer.frame = view.frame
         
         let dataOutput = AVCaptureVideoDataOutput()
+        
         dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
         captureSession.addOutput(dataOutput)
-        //let request = VNCoreMLRequest(model: <#T##VNCoreMLModel#>, completionHandler: <#T##VNRequestCompletionHandler?##VNRequestCompletionHandler?##(VNRequest, Error?) -> Void#>)
-        //VNImageRequestHandler(cgImage: <#T##CGImage#>, options: <#T##[VNImageOption : Any]#>)
     }
     
-    func captureOutput(_ output: AVCaptureOutput, didDrop sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        print("Camera tá funfando: ", Date())
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection)
+ {
+        
+        guard let buffer:CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            return
+        }
+        
+        guard let model = try? VNCoreMLModel(for: Resnet50(configuration: MLModelConfiguration()).model) else {
+            return
+        }
+        let request = VNCoreMLRequest(model: model){ (finishedReq, err) in
+            guard let results = finishedReq.results as? [VNClassificationObservation] else {return}
+            guard let firstObservation = results.first else {return}
+            print(firstObservation.identifier, firstObservation.confidence)
+        }
+        try? VNImageRequestHandler(cvPixelBuffer: buffer, options: [:]).perform([request])
     }
+    
 }
-
